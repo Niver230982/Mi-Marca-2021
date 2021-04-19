132185d9288a7f1d2da1fd84daf1ec148b88fb8b
maquina <maquina@maquina.pc>
2021-04-18 23:07:13 -0300

    agregado Anabel

--- app/src/main/java/com/niverarrigonni/mimarca/Marca.java
+++ app/src/main/java/com/niverarrigonni/mimarca/Marca.java
@@ -2,19 +2,27 @@ package com.niverarrigonni.mimarca;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.Objects;
 
 public class Marca extends AppCompatActivity {
 
 
+
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,20 +30,11 @@ public class Marca extends AppCompatActivity {
 
 
 
+
+
     }
 
-    //Botón Atrás
 
-
-   /// @Override
-  //  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-   //     switch (item.getItemId()){
-    //        case android.R.id.home:
-     //           finish();
-      //          return true;
-      //  }
-       // return super.onOptionsItemSelected(item);
-   // }
 
     //Botónes de marcado
 
@@ -47,12 +46,13 @@ public class Marca extends AppCompatActivity {
         Toast.makeText(this, "Iniciar descanso", Toast.LENGTH_LONG).show();
     }
 
+    public void Retorno(View view) {
+        Toast.makeText(this, "Fin de descanso", Toast.LENGTH_LONG).show();
+    }
+
     public void Salida(View view) {
         Toast.makeText(this, "Marcar salida", Toast.LENGTH_LONG).show();
     }
 
-    public void Atras(View view) {
-
-    }
 
 }
